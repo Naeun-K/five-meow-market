52693fe5de3041aabe6dae5809d54ae00dd1bb10
Choi Seon Ho <prefer3510@gmail.com>
2026-09-02 10:38:30 +0900

    feat: emotion 스타일로 파일 수정

--- src/components/updateinfo/updateInfo.jsx
+++ src/components/updateinfo/updateInfo.jsx
@@ -1,92 +1,67 @@
-import "./updateInfo.css";
-
-export default function UpdateInfo() {
+const UpdateInfo = () => {
     return (
-<div className="update-info">
-
-{/* 제목 영역 */}
-    <div className="update-header">
+        <Container>
+            <Header>
                 <h1>Update Info</h1>
-                <h3>집사가 변했어</h3>        
-                </div>
+                <p>집사가 변했어</p>
+            </Header>
 
-<div className="update-main">
+        <Main>
+            <InfoRow>
+                <span>닉네임</span>
+                <p>김오묘</p>
+            </InfoRow>
 
-    <div className="update-user">
-        <div className="update-name">
-            <span>닉네임</span>
-            <span>김오묘</span>
-        </div>
+            <InfoRow>
+                <span>이메일</span>
+                <p>ohmyoh@domain.com</p>
+            </InfoRow>
 
-{/* 이메일 영역 */}
-    <div className="update-email">
-        <span className="email">이메일</span>
-        <span className="email-name">ohmyoh@domain.com</span>
-      </div>
-    </div>
+            <FormGroup>
+                <label>새 비밀번호</label>
+                <InputWrapper>
+                    <Input type="password"
+                    placeholder="새 비밀번호를 입력해주세요."
+                    />
+                    <button>👁️</button>
+                </InputWrapper>
+            </FormGroup>
 
-{/* 비밀번호 영역 */}
-    <div className="update-password">
+            <FormGroup>
+                <label>새 비밀번호 확인</label>
+                <InputWrapper>
+                    <Input type="password"
+                    placeholder="새 비밀번호를 다시 입력해주세요."
+                    />
+                    <button>👁️</button>
+                </InputWrapper>
+            </FormGroup>
 
-        <div className="update-password-section">
-            <label>현재 비밀번호</label>
-            <div className="update-password-input">
-                <input type="password" placeholder="현재 비밀번호를 입력해주세요."/>
-                <span>👁️</span>
-            </div>
-        </div>
+            <FormGroup>
+                <label>휴대폰 번호</label>
+                <PhoneRow>
+                    <input 
+                     type="tel"
+                     placeholder="휴대폰 번호를 입력해주세요."
+                    />
+                    <VerifyButton>본인인증</VerifyButton>
+                </PhoneRow>
+            </FormGroup>
 
-        <div className="update-password-section">
-            <label>새 비밀번호</label>
+            <FormGroup>
+                <label>주소</label>
 
-            <div className="update-password-input">
-                <input
-                type="password" 
-                placeholder="새 비밀번호를 입력해주세요."/>
-                <span>👁️</span>
-            </div>        
-        </div>
+                <AddressRow>
+                    <input type="text" placeholder="우편번호"/>
+                    <AddressButton>우편번호 검색</AddressButton>
+                </AddressRow>
 
-        <div className="update-password-section">
-            <label>비밀번호 확인</label>
-            <div className="update-password-input">
-                <input type="password" placeholder="비밀번호를 확인해주세요."/>
-                <span>👁️</span>
-            </div>
-        </div>
+                    <input type="text" placeholder="주소를 입력해주세요."/>
+                    <input type="text" placeholder="상세 주소를 입력해주세요/"/>
+            </FormGroup>
 
-    </div>
-
-{/* 휴대폰 번호 영역 */}
-    <div className="update-phone">
-        <label>휴대폰 번호</label>
-
-        <div className="update-phone-input">
-            <input placeholder="휴대폰 번호를 입력해주세요"/>
-            <button>본인인증</button>
-        </div>
-    </div>
-
-{/* 주소 영역 */}
-
-    <div className="update-address">
-        <label>주소</label>
-        <div className="update-address-search">
-            <input placeholder="우편번호"/>
-            <button>우편번호 검색</button>
-        </div>
-
-        <input
-          className="update-address-input"
-          placeholder="주소를 입력해주세요"
-          />
-          </div>
-
-{/* 회원정보수정 버튼 */}
-
-          <button className="update-button">회원정보수정</button>
-    </div>
-
-    </div>
-    )
-}
+            <SubmitButton>회원정보수정</SubmitButton>
+        </Main>
+        </Container>
+    );
+};
