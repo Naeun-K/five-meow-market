e198314860a4fb385c4a73aa00ae87d6d383514a
Naeun-K <nek7148@gmail.com>
2026-09-11 15:52:42 +0900

    feat:화면변경 시 화면 최상단으로 자동 변경하는 로직 추가

--- src/App.jsx
+++ src/App.jsx
@@ -9,50 +9,54 @@ import NoticePage from "./pages/communitypage/NoticePage";
 import QnaPage from "./pages/communitypage/QnaPage";
 import ReviewPage from "./pages/communitypage/ReviewPage";
 import CartPage from "./pages/cartpage/CartPage";
+import ScrollToTop from "./routes/ScrollTop";
 
 function App() {
   return (
-    <Routes>
-      {/* 홈 */}
-      <Route path="/" element={<MainPage />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        {/* 홈 */}
+        <Route path="/" element={<MainPage />} />
 
-      {/* 상품 */}
-      <Route path="/products" element={<ProductList />}>
-        <Route index element={<ProductList />} />
-        {/* <Route path="/products/best" element={<ProductList />} /> */}
-      </Route>
+        {/* 상품 */}
+        <Route path="/products" element={<ProductList />}>
+          <Route index element={<ProductList />} />
+          {/* <Route path="/products/best" element={<ProductList />} /> */}
+        </Route>
 
-      {/* 회원 */}
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
+        {/* 회원 */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
-      {/* 마이페이지 */}
-      {/* <Route path="/mypage" element={<MyPage />} /> */}
-      <Route path="/mypage/edit" element={<UpdatePage />} />
-      {/* <Route path="/mypage/wishlist" element={<Wishlist />} /> */}
-      {/* <Route path="/mypage/orders" element={<OrderList />} /> */}
-      {/* <Route path="/mypage/orders/detail" element={<OrderList />} /> */}
+        {/* 마이페이지 */}
+        {/* <Route path="/mypage" element={<MyPage />} /> */}
+        <Route path="/mypage/edit" element={<UpdatePage />} />
+        {/* <Route path="/mypage/wishlist" element={<Wishlist />} /> */}
+        {/* <Route path="/mypage/orders" element={<OrderList />} /> */}
+        {/* <Route path="/mypage/orders/detail" element={<OrderList />} /> */}
 
-      {/* 장바구니 / 주문 */}
-      <Route path="/cart" element={<CartPage />} />
-      {/* <Route path="/checkout" element={<Checkout />} /> */}
+        {/* 장바구니 / 주문 */}
+        <Route path="/cart" element={<CartPage />} />
+        {/* <Route path="/checkout" element={<Checkout />} /> */}
 
-      {/* 공지사항 */}
-      <Route path="/community/notice" element={<NoticePage />} />
-      {/* Q&A */}
-      <Route path="/community/qna" element={<QnaPage />} />
-      {/* 리뷰 */}
-      <Route path="/community/review" element={<ReviewPage />} />
+        {/* 공지사항 */}
+        <Route path="/community/notice" element={<NoticePage />} />
+        {/* Q&A */}
+        <Route path="/community/qna" element={<QnaPage />} />
+        {/* 리뷰 */}
+        <Route path="/community/review" element={<ReviewPage />} />
 
-      {/* 취소 / 교환 / 반품 */}
-      {/* <Route path="/claims" element={<Claims />} /> */}
+        {/* 취소 / 교환 / 반품 */}
+        {/* <Route path="/claims" element={<Claims />} /> */}
 
-      {/* 고객센터 */}
-      {/* <Route path="/support" element={<Claims />} /> */}
+        {/* 고객센터 */}
+        {/* <Route path="/support" element={<Claims />} /> */}
 
-      {/* 404 */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
