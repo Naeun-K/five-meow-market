6227d2a35f0f675f05402141057a4402ffba4d46
Naeun-K <nek7148@gmail.com>
2026-09-14 13:53:39 +0900

    chore: 배포 중 문제로 로컬 오류 나서 해결

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/five-meow-market/",
-
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/five-meow-market/" : "/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
     }),
   ],
-});
+}));
