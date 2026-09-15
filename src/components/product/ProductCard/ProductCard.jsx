2c7de1651c0c6d213c837487eb398632039b54eb
sooy1248@gmail.com <sooy1248@gmail.com>
2026-09-15 10:21:10 +0900

    feat: 상품 카드 및 네비게이션 인터랙션 개선

--- src/components/product/ProductCard/ProductCard.jsx
+++ src/components/product/ProductCard/ProductCard.jsx
@@ -3,11 +3,22 @@
 import * as S from "./ProductCard.style";
 import HeartButton from "../HeartButton/HeartButton";
 
-const ProductCard = ({ image, name, badge, showHeart = true }) => {
+const ProductCard = ({
+  image,
+  name,
+  badge,
+  showHeart = true,
+  hover = true,
+}) => {
   return (
-    <article css={S.card}>
+    <article css={S.card(hover)}>
       {image && (
-        <img css={S.productImage} src={image} alt={name} loading="lazy" />
+        <img
+          css={S.productImage(hover)}
+          src={image}
+          alt={name}
+          loading="lazy"
+        />
       )}
 
       {badge && <span css={S.badge(badge)}>{S.badgeText[badge]}</span>}
