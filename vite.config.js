386b7c89f21c30d4e42241a061d23885ec7de3e3
Naeun-K <nek7148@gmail.com>
2026-09-15 16:47:59 +0900

    chore:config 환경 세팅 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/five-meow-market/" : "/",
+export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
     }),
   ],
-}));
+});
